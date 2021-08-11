1d87e399b5127ba535c27ce2945ad492dd1a41fe
Dan O'Leary <olearydaniel@me.com>
2021-08-11 11:27:59 -0500

    Initial Commit

---
+++ DantesInferno/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DantesInferno
+//
+//  Created by Daniel O'Leary on 8/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DantesInferno/DantesInfernoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DantesInfernoApp.swift
+//  DantesInferno
+//
+//  Created by Daniel O'Leary on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DantesInfernoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
